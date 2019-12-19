6ed9f9426471a4974ba7ee6977250d3656f55dee
xpeng2333 <2544627510@qq.com>
2019-12-19 19:37:26 +0800

    test

--- src/csv2neodata.py
+++ src/csv2neodata.py
@@ -1,18 +1,9 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
 
-import pandas as pd
 import csv
 import os
-import re
-'''
-# 读取三元组文件
-h_r_t_name = [":START_ID", "role", ":END_ID"]
-h_r_t = pd.read_table(
-    os.getcwd() + "/../data/pure_baike_triples.txt0", quoting=3, decimal="\t", names=h_r_t_name)
-print(h_r_t.info())
-print(h_r_t.head())
-'''
+
 h_r_t = {":START_ID": [], "role": [], ":END_ID": []}
 for line in open(os.getcwd() + "/../data/pure_baike_triples.txt0"):
     triple = line.strip().split("\t")
@@ -32,50 +23,51 @@ for i in entity_h:
     entity.add(i)
 entity_t_dict = dict()
 for i in entity:
-    entity_t_dict[i] = 2
+    entity_t_dict[i] = 11
 for i in entity_t:
     entity_t_dict[i] = entity_t_dict.get(i, 0)+1
 for i in entity_t_dict:
-    if entity_t_dict[i] > 1:
+    if entity_t_dict[i] > 10:
         entity.add(i)
     else:
         prop.add(i)
 print(len(entity))
+del entity_t_dict
 # 保存节点文件
 csvf_entity = open(os.getcwd() + "/../data/entity.csv",
                    "w", newline='', encoding='utf-8')
-# w_entity = csv.writer(csvf_entity)
-# 实体ID，要求唯一，名称，LABEL标签，可自己不同设定对应的标签
-# w_entity.writerow(("entity:ID", "name"))
+
 csvf_entity.write("entity:ID, name"+"\n")
 entity = list(entity)
 entity_dict = {}
 for i in range(len(entity)):
-    #w_entity.writerow(("e" + str(i), "\""+str(entity[i])+"\""))
-    csvf_entity.write("e"+str(i)+",<"+entity[i]+">\n")
+    csvf_entity.write("e"+str(i)+","+entity[i]+"\n")
     entity_dict[entity[i]] = "e"+str(i)
 csvf_entity.close()
 del entity
 csvf_relation = open(os.getcwd() + "/../data/relation.csv",
                      "w", newline='', encoding='utf-8')
-#w_relation = csv.writer(csvf_relation)
-#w_relation.writerow((":START_ID", ":END_ID", ":TYPE"))
 csvf_relation.write(":START_ID,:END_ID,:TYPE\n")
+propf = open(os.getcwd() + "/../data/prop.txt", "w")
 for h, t, r in zip(h_r_t[':START_ID'], h_r_t[':END_ID'], h_r_t['role']):
     if t in prop:
+        propf.write(h+","+r+","+t+"\n")
         continue
     else:
-        #w_relation.writerow((entity_dict[h], entity_dict[t], r))
         csvf_relation.write(entity_dict[h]+"," + entity_dict[t]+"," + r+"\n")
 csvf_relation.close()
-'''
-# 生成关系文件，起始实体ID，终点实体ID，要求与实体文件中ID对应，:TYPE即为关系
-h_r_t[':START_ID'] = h_r_t[':START_ID'].map(entity_dict)
-h_r_t[':END_ID'] = h_r_t[':END_ID'].map(entity_dict)
-h_r_t[":TYPE"] = h_r_t['role']
-h_r_t.pop('role')
-h_r_t.to_csv(os.getcwd() + "/../data/roles.csv", index=False)
-'''
+for line0 in open(os.getcwd() + "/../data/baike_triples.txt"):
+    triple = line0.strip().split("\t")
+    if len(triple) != 3 or triple[1] != "BaiduCARD":
+        continue
+    line = line0.replace("<a>", "")
+    line = line.replace("</a>", "")
+    line = line.replace("\"\"", "")
+    line = line.replace("\\\"", "”")
+    line = line.replace("\"", "”")
+    line = line.replace(",", "，")
+    propf.write(line)
+propf.close()
 '''
 ./neo4j-import --into /home/xiaopeng/Downloads/neo4j-community-3.5.13/data/databases/graph.db --nodes /home/xiaopeng/Desktop/AutoKnowledge/data/entity.csv --relationships /home/xiaopeng/Desktop/AutoKnowledge/data/roles.csv
 '''
