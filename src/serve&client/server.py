5ed6e8c3cbbcf16f45fc997ecd898f4a90fdcd1e
xpeng2333 <2544627510@qq.com>
2020-07-16 21:32:53 +0800

    fix bug

--- src/serve&client/server.py
+++ src/serve&client/server.py
@@ -2,72 +2,95 @@ import threading
 import socket
 from time import strftime, localtime
 import json
-
+import numpy as np
 #############################
 import os
 from py2neo import Graph, Node, Relationship, NodeMatcher, PropertyDict
 import time
 import random
 
+
 id2entityDic = dict()
-entity2idDic = dict()
-for line in open(os.getcwd() + "/../data/entity.csv"):
-    triple = line.strip().split(",")
-    id2entityDic[triple[0]] = triple[1]
-    entity2idDic[triple[1]] = triple[0]
+entity2idDic = np.load("../../data/entity_id.npy",
+                       allow_pickle=True).item()  # 测试,e12
+
+for item in entity2idDic:
+    id2entityDic[entity2idDic[item][1:]] = item
+
 graph = Graph('http://localhost:7474', username='neo4j', password='123321')
 
 
 def hr2t(entityID, relationType):
-    return "MATCH p=(h)-[r:`"+relationType+"`]->(t) where h.entity=\""+entityID+"\" RETURN t"
+    return "MATCH p=(h)-[r:`"+relationType+"`]->(t) where id(h)="+entityID+" RETURN t.` name`"
 
 
 def ht2r(entityID_h, entityID_t):
-    return "MATCH p=(h)-[r]->(t) where h.entity=\""+entityID_h+"\" and t.entity=\""+entityID_t + "\" RETURN r"
+    return "MATCH p=(h)-[r]->(t) where id(h)="+entityID_h+" and id(t)="+entityID_t + " RETURN r"
 
 
 def rt2h(entityID, relationType):
-    return "MATCH p=(h)-[r:`"+relationType+"`]->(t) where t.entity=\""+entityID+"\" RETURN h"
+    return "MATCH p=(h)-[r:`"+relationType+"`]->(t) where id(t)="+entityID+" RETURN h.` name`"
 
 
 def h2r(entityID):
-    return "MATCH p=(h)-[r]->(t) where h.entity=\""+entityID+"\" RETURN r"
+    return "MATCH p=(h)-[r]->(t) where id(h)="+entityID+" RETURN r"
 
 
 def t2r(entityID):
-    return "MATCH p=(h)-[r]->(t) where t.entity=\""+entityID+"\" RETURN r"
+    return "MATCH p=(h)-[r]->(t) where id(t)="+entityID+" RETURN r"
 
+
+def id2name(entityID):
+    p = graph.run("match (n) where id(n) = "+entityID+" return n.` name`")
+    for item in p:
+        return item.values()[0]
 # （）的{关系}是{尾实体}?
 
 
 def findHead(entityID, count=1):
+    print("start")
+    print(entityID)
     relations = []
     p = graph.run(h2r(entityID))
     for reltion in p:
         relations.append(reltion.values()[0].__class__.__name__)
     selectRelation = relations[random.randint(0, len(relations)-1)]
+    print(relations)
+    print(selectRelation)
     tails = []
     p = graph.run(hr2t(entityID, selectRelation))
     for tail in p:
-        tails.append(id2entityDic[tail.values()[0]["entity"]])
+        tails.append(tail.values()[0])
+    print(tails)
     selectTail = tails[random.randint(0, len(tails)-1)]
     # print(id2entityDic[entityID]+"的"+selectRelation+"是"+selectTail)
+    option = ['a', 'b', 'c', 'd']
+    option[0] = id2name(entityID)
     question = "（   ）的"+selectRelation+"是"+selectTail
-    return [question, [id2entityDic[entityID], 'b', 'c', 'd']], ['A']
+    for i in range(1, 4):
+        randID = str(random.randint(0, 30000))
+        option[i] = id2name(randID)
+    return [question, option], ['A']
+
 # {头实体}的{关系}是()?单选 || {头实体}的{关系}有()?
 
 
 def findTail(entityID):
+    print("start")
+    print(entityID)
     relations = []
     p = graph.run(h2r(entityID))
     for reltion in p:
         relations.append(reltion.values()[0].__class__.__name__)
     selectRelation = relations[random.randint(0, len(relations)-1)]
+    print(relations)
+    print(selectRelation)
     p = graph.run(hr2t(entityID, selectRelation))
     tails = set()
     for tail in p:
-        tails.add(id2entityDic[tail.values()[0]["entity"]])
+        tails.add(tail.values()[0])
     tails = list(tails)
+    print(tails)
     flag = False
     if len(tails) > 1:
         flag = True
@@ -75,30 +98,38 @@ def findTail(entityID):
     if count > 4:
         count = 4
     if flag:
-        question = id2entityDic[entityID]+"的"+selectRelation+"有(   )"
+        question = id2name(entityID)+"的"+selectRelation+"有(   )"
     else:
-        question = id2entityDic[entityID]+"的"+selectRelation+"是(   )"
+        question = id2name(entityID)+"的"+selectRelation+"是(   )"
     option = ['a', 'b', 'c', 'd']
     answer = []
+    tmpIDs = []
     for i in range(count):
-        selectTail = tails[random.randint(0, len(tails)-1)]
+        tmpID = random.randint(0, len(tails)-1)
+        while tmpID in tmpIDs:
+            tmpID = random.randint(0, len(tails)-1)
+        selectTail = tails[tmpID]
         option[i] = selectTail
         answer.append(chr(ord('A')+i))
+    for i in range(4-count):
+        randID = str(random.randint(0, 30000))
+        option[count+i] = id2name(randID)
     print(answer)
     return [question, option], answer
 
 
-def get_question(entity=""):
+def get_question():
     while True:
         try:
             classID = random.randint(0, 1)
-            entityID = "e"+str(random.randint(0, 5000000))
+            entityID = str(random.randint(0, 30000))
             if classID:
                 return findTail(entityID)
             else:
                 return findHead(entityID)
         except:
             pass
+
 ##############################
 
 
@@ -129,7 +160,7 @@ def tcp_send(sock):
 
     print('Connection from %s:%s closed.' % addr)
 '''
-professions = ['aa', 'b', 'c']
+professions = ['a', 'b', 'c']
 
 '''
 def get_question():
